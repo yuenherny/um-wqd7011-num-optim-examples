fbd3209006e0ab0756d481030e5c1b2a81b65429
yuenhern96@gmail.com <yuenhern.yu@gmail.com>
2023-05-12 03:10:32 +0800

    Add Newton and Quasi Newton example

--- scripts/newton.m
+++ scripts/newton.m
@@ -1,5 +1,5 @@
 clc, clear;
-% steepest descent
+% newton method
 
 % install first: https://gnu-octave.github.io/packages/symbolic/
 pkg load symbolic % install first
@@ -14,7 +14,12 @@ c0 = [1;2];
 gradient_mat = gradient(f)
 df = function_handle(gradient_mat)
 
-% calculate direction at first point
-p = -df(c0(1),c0(2))
-c1 = c0 + p
+% calculate hessian matrix
+% ref: https://octave.sourceforge.io/symbolic/function/@sym/hessian.html
+hessian_mat = hessian(f)
+ddf = function_handle(hessian_mat)
+is_pos_def = isdefinite(ddf(c0(1),c0(2)))
 
+% calculate direction at first point
+p = -inv(ddf(c0(1),c0(2))) * df(c0(1),c0(2))
+c1 = c0 + p
